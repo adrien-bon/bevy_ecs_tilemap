ebd90fa428eec2d512d76d4425d8b9a4203ed5ea
Adrien Bon <63343541+adrien-bon@users.noreply.github.com>
2024-07-24 10:36:11 +0000

    dev: update devcontainer extras.sh

--- .devcontainer/extras.sh
+++ .devcontainer/extras.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-## TODO: add any extra personalization here
-
-exit 0
+# Install extras packages for BEVY
+apt install --yes \
+    libx11-dev \
+    libasound2-dev \
+    libudev-dev \
+    libxkbcommon-x11-0 \
+    mesa-vulkan-drivers \
+    libwayland-dev \
+    libxkbcommon-dev
